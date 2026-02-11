49a5b5ecbe55f32155154dbf373f2e77b1dbd12b
jerrie <Jerrie.77@hotmail.se>
2026-02-11 10:41:08 +0100

    -

--- backend/src/main/java/se/foodassistant/backend/Dto/RecipeDto.java
+++ backend/src/main/java/se/foodassistant/backend/Dto/RecipeDto.java
@@ -1,5 +1,36 @@
 package se.foodassistant.backend.Dto;
 
-public class RecipeDto {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+public class RecipeDto {
+    @JsonProperty("title")
+    String title;
+    @JsonProperty("Instructions")
+    String Instructions;
+    @JsonProperty("cookingTime")
+    Integer cockingTime;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getInstructions() {
+        return Instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        Instructions = instructions;
+    }
+
+    public Integer getCockingTime() {
+        return cockingTime;
+    }
+
+    public void setCockingTime(Integer cockingTime) {
+        this.cockingTime = cockingTime;
+    }
 }
