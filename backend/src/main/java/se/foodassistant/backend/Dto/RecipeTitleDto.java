3367477ab569543939e60a29928856bf582999ec
Luckysnowwolf <179121658+Luckysnowwolf@users.noreply.github.com>
2026-02-16 09:38:02 +0100

    Update RecipeTitleDto.java

--- backend/src/main/java/se/foodassistant/backend/Dto/RecipeTitleDto.java
+++ backend/src/main/java/se/foodassistant/backend/Dto/RecipeTitleDto.java
@@ -1,17 +1,12 @@
 package se.foodassistant.backend.Dto;
 
 public class RecipeTitleDto {
-    private final int id;
     private final String title;
 
-    public RecipeTitleDto(int id, String title) {
-        this.id = id;
+    public RecipeTitleDto(String title) {
         this.title = title;
     }
 
-    public int getId() {
-        return id;
-    }
     public String getTitle() {
         return title;
     }
