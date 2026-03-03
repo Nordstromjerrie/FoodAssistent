ccdd742c2b0c7d6a4947d66e071dee08c4a61b9f
Luckysnowwolf <179121658+Luckysnowwolf@users.noreply.github.com>
2026-03-03 10:49:21 +0100

    added v10 and 11

--- backend/src/main/java/se/foodassistant/backend/Entity/User.java
+++ backend/src/main/java/se/foodassistant/backend/Entity/User.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import java.util.List;
 
 @Entity
-public class UserEntity {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
