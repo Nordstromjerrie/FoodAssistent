ceccaabe2313af3e88eef0cd66adcc6966523e7e
Emil9608 <emil__96@hotmail.com>
2026-04-29 12:23:40 +0200

    Fixa tailwind till recipes/page och skapa ett grid för recept display. Ändra färger i navbar och footer från vit till orange

--- Frontend/components/footer.tsx
+++ Frontend/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-black border-t border-orange-500 mt-10">
+    <footer className="bg-black border-t border-orange-400 mt-10">
       <div className="max-w-6xl mx-auto px-6 py-6 flex flex-col items-center gap-4">
 
         
@@ -11,7 +11,7 @@ export default function Footer() {
           <span className="hover:text-orange-400 transition"> Emil</span>
         </div>
 
-        <div className="w-full h-px bg-orange-500 opacity-30"></div>
+        <div className="w-full h-px bg-orange-400 opacity-30"></div>
 
         <p className="text-gray-400 text-xs">
           © {new Date().getFullYear()} FoodAssistant 
--- Frontend/components/navbar.tsx
+++ Frontend/components/navbar.tsx
@@ -3,22 +3,22 @@ import Link from "next/link";
 
 export default function navbar() {
   return (
-    <nav className="bg-black border-b border-orange-500 px-6 py-4 flex justify-between items-center">
+    <nav className="bg-black border-b border-orange-400 px-6 py-4 flex justify-between items-center">
       
       {/* LOGO */}
-      <div className="text-white text-xl font-bold">
-        FoodAssistant
+      <div className="text-orange-400 text-x1 font-bold">
+        Food Assistant
       </div>
 
       {/* LINKS */}
       <div className="flex gap-6">
         
 
-        <Link href="/recipes-client" className="text-white hover:text-orange-400">
+        <Link href="/recipes-client" className="text-orange-400 hover:text-orange-400">
           Search recipes 
         </Link>
 
-        <Link href="/recipes" className="text-white hover:text-orange-400">
+        <Link href="/recipes" className="text-orange-400 hover:text-orange-400">
           All recipes 
         </Link>
       </div>
